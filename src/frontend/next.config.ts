35f4174b196676717e647613c105cda3ba4e3126
jbpenrath <jb.penrath@gmail.com>
2025-04-16 21:22:14 +0200

    🔧(frontend) disable TypeScript build error
    Temporarily ignore TypeScript build errors in the Next.js
    configuration to facilitate development until all model types are defined.

--- src/frontend/next.config.ts
+++ src/frontend/next.config.ts
@@ -4,6 +4,10 @@ const nextConfig: NextConfig = {
   output: "export",
   debug: process.env.NODE_ENV === "development",
   reactStrictMode: false,
+  // FIXME: Ignore build errors until we have all the model types
+  typescript: {
+    ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig;
