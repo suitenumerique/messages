18304a50f9b830cdf3bdee569c5e3aebfec80ecb
jbpenrath <jb.penrath@gmail.com>
2025-04-16 21:51:19 +0200

    ✨(frontend) add AuthenticatedView
    If an unauthenticated user tries to go to a view requiring
    authentication, it should be redirect to the homepage

--- src/frontend/src/features/layouts/components/main/index.tsx
+++ src/frontend/src/features/layouts/components/main/index.tsx
@@ -3,18 +3,21 @@ import { HeaderRight } from "../header";
 import { MailboxPanel } from "@/features/layouts/components/mailbox-panel";
 import { PropsWithChildren } from "react";
 import { GlobalLayout } from "../global/GlobalLayout";
+import AuthenticatedView from "./authenticated-view";
 
-export const MainLayout = ({ children }: PropsWithChildren<{}>) => {
+export const MainLayout = ({ children }: PropsWithChildren) => {
     return (
         <GlobalLayout>
-            <KitMainLayout
-                enableResize
-                leftPanelContent={<MailboxPanel />}
-                icon={<img src="/images/app-logo.svg" alt="logo" height={32} />}
-                rightHeaderContent={<HeaderRight />}
-            >
-                {children}
-            </KitMainLayout>
+            <AuthenticatedView>
+                <KitMainLayout
+                    enableResize
+                    leftPanelContent={<MailboxPanel />}
+                    icon={<img src="/images/app-logo.svg" alt="logo" height={32} />}
+                    rightHeaderContent={<HeaderRight />}
+                >
+                    {children}
+                </KitMainLayout>
+            </AuthenticatedView>
         </GlobalLayout>
     )
 }
