2d14439ae3154cd2e7b16c1a6651755a6e76b57c
jbpenrath <jb.penrath@gmail.com>
2025-04-17 10:46:13 +0200

    ✨(frontend) display selected mailbox in mailbox-panel
    Now that we are able to retriver user mailboxes, we want to display
    the selected mailbox in the mailbox-panel. First we assume that the
    user will always have a single mailbox so we always select the first
    mailbox on fetch.

--- src/frontend/src/features/layouts/components/mailbox-panel/index.tsx
+++ src/frontend/src/features/layouts/components/mailbox-panel/index.tsx
@@ -1,15 +1,24 @@
-import { HorizontalSeparator } from "@gouvfr-lasuite/ui-kit"
+import { HorizontalSeparator, Spinner } from "@gouvfr-lasuite/ui-kit"
 import { MailboxPanelActions } from "./components/mailbox-actions"
 import { MailboxList } from "./components/mailbox-list"
+import { useMailboxContext } from "@/features/mailbox/provider";
 
 export const MailboxPanel = () => {
+    const { selectedMailbox, status} = useMailboxContext();
     return (
         <div className="mailbox-panel">
             <div className="mailbox-panel__header">
                 <MailboxPanelActions />
                 <HorizontalSeparator withPadding={false} />
             </div>
-            <MailboxList />
+            {!selectedMailbox || status.mailboxes === "pending" ? <Spinner /> : 
+            (
+                <>
+                    {/* FIXME: For now we consider user has always only one mailbox */}
+                    <h2 className="mailbox-panel__mailbox-title">{selectedMailbox.email}</h2>
+                    <MailboxList />
+                </>
+            )}
         </div>
     )
 }
