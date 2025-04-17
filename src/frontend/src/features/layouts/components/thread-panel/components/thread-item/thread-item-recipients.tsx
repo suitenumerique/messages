4d7d652bf5c178ea7770330eb18aaf5a36624927
jbpenrath <jb.penrath@gmail.com>
2025-04-17 10:46:59 +0200

    ✨(frontend) display threads
    Now that we are able to fetch threads, we replace our fake date by real ones!

---
+++ src/frontend/src/features/layouts/components/thread-panel/components/thread-item/thread-item-recipients.tsx
@@ -0,0 +1,24 @@
+import { Thread } from "@/features/api/gen/models"
+
+type ThreadItemRecipientsProps = {
+    recipients: Thread["recipients"]
+}
+
+export const ThreadItemRecipients = ({ recipients }: ThreadItemRecipientsProps) => {
+    return (
+        <div className="thread-item__recipients-container">
+            <ul className="thread-item__recipients">
+                {recipients.map((recipient) => (
+                    <li className="thread-item__recipient" key={recipient.id}>
+                        <strong>{recipient.name || recipient.email}</strong>
+                    </li>
+                ))}
+            </ul>
+            {recipients.length > 1 && (
+                <span className="thread-item__recipients-count">
+                    {recipients.length}
+                </span>
+            )}
+        </div>
+    )
+}
--- src/frontend/src/features/layouts/components/thread-panel/index.tsx
+++ src/frontend/src/features/layouts/components/thread-panel/index.tsx
@@ -1,21 +1,13 @@
+import { useMailboxContext } from "@/features/mailbox/provider";
 import { ThreadItem } from "./components/thread-item";
 
-// TODO: Use real data
-const TMP_THREADS = [...Array(100)].map((_, index) => ({
-    id: index.toString(),
-    has_attachments:  Math.random() > 0.5,
-    is_unread: Math.random() > 0.5,
-    is_important:  Math.random() > 0.5,
-    sent_at: index === 0 ? "2025-04-15T00:00:00Z" : new Date(Date.now() - ((1000 * 60 * 60 * 24 * 182)/10) * index).toISOString(),
-    sender_name: "John Doe",
-    subject: "Subject "+index,
-}));
 
 export const ThreadPanel = () => {
+    const { threads } = useMailboxContext();
     return (
         <div className="thread-panel">
             {
-                TMP_THREADS.map((thread) => (
+                threads?.results.map((thread) => (
                     <ThreadItem key={thread.id} thread={thread} />
                 ))
             }
