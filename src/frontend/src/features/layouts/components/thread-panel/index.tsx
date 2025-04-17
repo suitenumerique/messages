9501d729b6b1e6665e6fd9c8a53d844dacf9741e
jbpenrath <jb.penrath@gmail.com>
2025-04-17 11:24:25 +0200

    ✨(frontend) enhance ThreadPanel with loading and empty states
    - Added loading spinner when threads are being fetched.
    - Display a message when no threads are available

--- src/frontend/src/features/layouts/components/thread-panel/index.tsx
+++ src/frontend/src/features/layouts/components/thread-panel/index.tsx
@@ -1,16 +1,36 @@
 import { useMailboxContext } from "@/features/mailbox/provider";
 import { ThreadItem } from "./components/thread-item";
-
+import { Spinner } from "@gouvfr-lasuite/ui-kit";
+import { useTranslation } from "react-i18next";
 
 export const ThreadPanel = () => {
-    const { threads } = useMailboxContext();
+    const { threads, status } = useMailboxContext();
+    const { t } = useTranslation();
+
+    if (status.threads === "pending") {
+        return (
+            <div className="thread-panel thread-panel--loading">
+                <Spinner />
+            </div>
+        );
+    }
+
+    if (!threads?.results.length) {
+        return (
+            <div className="thread-panel thread-panel--empty">
+                <div>
+                    <span className="material-icons">mail</span>
+                    <p>{t('no_threads')}</p>
+                </div>
+            </div>
+        );
+    }
+
     return (
         <div className="thread-panel">
-            {
-                threads?.results.map((thread) => (
-                    <ThreadItem key={thread.id} thread={thread} />
-                ))
-            }
+            {threads.results.map((thread) => (
+                <ThreadItem key={thread.id} thread={thread} />
+            ))}
         </div>
-    )
+    );
 }
