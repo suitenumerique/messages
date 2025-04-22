e2cc4035d0cf14d2f291811cc1f6e458721162e9
jbpenrath <jb.penrath@gmail.com>
2025-04-22 11:54:53 +0200

    🚨(frontend) do not lint generated api client files
    As orval is generated api clients, we don't want lint this part of the codebase

--- src/frontend/eslint.config.mjs
+++ src/frontend/eslint.config.mjs
@@ -12,6 +12,9 @@ const compat = new FlatCompat({
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
+    ignores: ["src/features/api/gen/**/*.ts"],
+  },
+  {
     rules: {
       "react-hooks/exhaustive-deps": "off",
     },
