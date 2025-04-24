e5df50f6a9d1e28f9d394aa9213bedf95cd986d9
jbpenrath <jb.penrath@gmail.com>
2025-04-24 10:44:59 +0200

    💄(frontend) display mailbox in a select
    In order to improve layout, we display mailbox in a select.
    It prepares the future by allowing to list all mailbox a user has access

--- src/frontend/src/features/layouts/components/mailbox-panel/index.tsx
+++ src/frontend/src/features/layouts/components/mailbox-panel/index.tsx
@@ -2,9 +2,21 @@ import { HorizontalSeparator, Spinner } from "@gouvfr-lasuite/ui-kit"
 import { MailboxPanelActions } from "./components/mailbox-actions"
 import { MailboxList } from "./components/mailbox-list"
 import { useMailboxContext } from "@/features/mailbox/provider";
+import { Select } from "@openfun/cunningham-react";
+import { useTranslation } from "react-i18next";
 
 export const MailboxPanel = () => {
-    const { selectedMailbox, status} = useMailboxContext();
+    const { t } = useTranslation();
+    const { selectedMailbox, mailboxes, status} = useMailboxContext();
+
+    const getMailboxOptions = () => {
+        if(!mailboxes) return [];
+        return mailboxes.map((mailbox) => ({
+            label: mailbox.email,
+            value: mailbox.id
+        }));
+    }
+
     return (
         <div className="mailbox-panel">
             <div className="mailbox-panel__header">
@@ -15,7 +27,16 @@ export const MailboxPanel = () => {
             (
                 <>
                     {/* FIXME: For now we consider user has always only one mailbox */}
-                    <h2 className="mailbox-panel__mailbox-title">{selectedMailbox.email}</h2>
+                    <Select
+                        className="mailbox-panel__mailbox-title"
+                        options={getMailboxOptions()}
+                        defaultValue={selectedMailbox.id}
+                        label={t('mailbox')}
+                        clearable={false}
+                        compact
+                        fullWidth
+                        showLabelWhenSelected={false}
+                    />
                     <MailboxList />
                 </>
             )}
