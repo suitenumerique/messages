29b7b183a054419079be974110b38e13924345cc
jbpenrath <jb.penrath@gmail.com>
2025-04-28 09:10:24 +0200

    ✨(frontend) allow to force thread refetch
    Add an actionbar in the ThreadPanel with a fisrt action button
    which to refresh thread list.

--- src/frontend/src/features/layouts/components/thread-panel/index.tsx
+++ src/frontend/src/features/layouts/components/thread-panel/index.tsx
@@ -2,9 +2,11 @@ import { useMailboxContext } from "@/features/mailbox/provider";
 import { ThreadItem } from "./components/thread-item";
 import { Spinner } from "@gouvfr-lasuite/ui-kit";
 import { useTranslation } from "react-i18next";
+import Bar from "@/features/ui/components/bar";
+import { Button, Tooltip } from "@openfun/cunningham-react";
 
 export const ThreadPanel = () => {
-    const { threads, status } = useMailboxContext();
+    const { threads, status, refetchMailboxes } = useMailboxContext();
     const { t } = useTranslation();
 
     if (status.threads === "pending") {
@@ -28,6 +30,17 @@ export const ThreadPanel = () => {
 
     return (
         <div className="thread-panel">
+            <Bar>
+                <Tooltip content={t('actions.refresh')}>
+                    <Button
+                        onClick={refetchMailboxes}
+                        icon={<span className="material-icons">refresh</span>}
+                        color="tertiary-text"
+                        size="small"
+                        aria-label={t('actions.refresh')}
+                    />
+                </Tooltip>
+            </Bar>
             {threads.results.map((thread) => (
                 <ThreadItem key={thread.id} thread={thread} />
             ))}
