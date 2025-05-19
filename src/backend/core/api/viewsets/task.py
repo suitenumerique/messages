d0c4b64a1acdd96119e2b22c1e539b148c3e51a7
jbpenrath <jb.penrath@gmail.com>
2025-05-19 21:11:17 +0200

    💚(ci) add job check-api-state
    Add a job to ensure that api schema and client
    are up-to-date.

--- src/backend/core/api/viewsets/task.py
+++ src/backend/core/api/viewsets/task.py
@@ -34,7 +34,9 @@ logger = logging.getLogger(__name__)
         200: inline_serializer(
             name="TaskStatusResponse",
             fields={
-                "status": drf_serializers.ChoiceField(choices=celery_states.ALL_STATES),
+                "status": drf_serializers.ChoiceField(
+                    choices=sorted(celery_states.ALL_STATES)
+                ),
                 "result": drf_serializers.JSONField(allow_null=True),
                 "error": drf_serializers.CharField(allow_null=True),
             },
