6eeda8cd17ba271af7c7359e7db371803b26e177
jbpenrath <jb.penrath@gmail.com>
2025-05-28 09:44:26 +0200

    ✨(frontend) display size in AttachmentItem
    Display the size of an attachment. Furthermore we also
    display attachments total size in the ThreadMessage (Previously
    this info was only dispalyed in composer)

--- src/frontend/src/features/layouts/components/thread-view/components/thread-attachment-list/index.tsx
+++ src/frontend/src/features/layouts/components/thread-view/components/thread-attachment-list/index.tsx
@@ -1,17 +1,22 @@
 import { Attachment } from "@/features/api/gen/models/attachment";
 import { AttachmentItem } from "./attachment-item";
 import { useTranslation } from "react-i18next";
+import { AttachmentHelper } from "@/features/utils/attachment-helper";
 type AttachmentListProps = {
     attachments: readonly Attachment[]
 }
 
 export const AttachmentList = ({ attachments }: AttachmentListProps) => {
-    const { t } = useTranslation();
+    const { t, i18n } = useTranslation();
     
     return (
         <section className="thread-attachment-list">
             <header className="thread-attachment-list__header">
-                <p className="m-0"><strong>{t("attachments.counter", { count: attachments.length })}</strong></p>
+                <p>
+                    <strong>{t("attachments.counter", { count: attachments.length })}</strong>
+                    {' '}
+                    ({AttachmentHelper.getFormattedTotalSize(attachments, i18n.language)})
+                </p>
             </header>
             <div className="thread-attachment-list__body">
                 {attachments.map((attachment) => <AttachmentItem key={attachment.id} attachment={attachment} />)}
