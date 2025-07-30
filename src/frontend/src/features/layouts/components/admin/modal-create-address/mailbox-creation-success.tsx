9dbc07cf004ecd1d34d3b61e80d1ef534f36f1e0
jbpenrath <jb.penrath@gmail.com>
2025-07-30 12:20:24 +0200

    ✨(front) add button to copy credential on mailbox creation
    Add a button to quicly copy credentials when a new mailbox has
    been copied.

--- src/frontend/src/features/layouts/components/admin/modal-create-address/mailbox-creation-success.tsx
+++ src/frontend/src/features/layouts/components/admin/modal-create-address/mailbox-creation-success.tsx
@@ -2,6 +2,7 @@ import { MailboxAdminCreate } from "@/features/api/gen";
 import { Banner } from "@/features/ui/components/banner";
 import { Icon, IconType } from "@gouvfr-lasuite/ui-kit";
 import { Button } from "@openfun/cunningham-react";
+import { useMemo, useState } from "react";
 import { Trans, useTranslation } from "react-i18next";
 
 type MailboxCreationSuccessProps = {
@@ -13,10 +14,27 @@ type MailboxCreationSuccessProps = {
 export const MailboxCreationSuccess = ({ type, mailbox, onClose }: MailboxCreationSuccessProps) => {
     const { t } = useTranslation();
     const mailboxAddress = mailbox.local_part + "@" + mailbox.domain_name;
+    const [clipboardState, setClipboardState] = useState<'idle' | 'copied' | 'error'>('idle');
+
+    const credentialText = useMemo(() => {
+      if (!mailbox.one_time_password) return '';
+      return t('create_address_modal.success.credential_text', { id: mailboxAddress, password: mailbox.one_time_password });
+    }, [mailbox]);
+
+    const handleCopyToClipboard = () => {
+      try {
+        navigator.clipboard.writeText(credentialText);
+        setClipboardState('copied');
+      } catch (error) {
+        setClipboardState('error');
+        console.error(error);
+      }
+      setTimeout(() => setClipboardState('idle'), 1337);
+    };
 
     return (
         <div className="modal-create-address-success">
-          <div className="importer-completed__description">
+          <div className="modal-create-address__description">
                 <div className="success-img-container">
                   <img src="/images/welcome.webp" alt="" />
                 </div>
@@ -37,12 +55,23 @@ export const MailboxCreationSuccess = ({ type, mailbox, onClose }: MailboxCreati
                       {
                         mailbox.one_time_password ? (
                           <>
-                            <dl>
-                              <dt>{t('create_address_modal.success.credential_identity')}</dt>
-                              <dd>{mailbox.local_part}@{mailbox.domain_name}</dd>
-                              <dt>{t('create_address_modal.success.credential_password')}</dt>
-                              <dd>{mailbox.one_time_password}</dd>
-                            </dl>
+                            <div className="modal-create-address__credentials">
+                              <dl>
+                                <dt>{t('create_address_modal.success.credential_identity')}</dt>
+                                <dd>{mailbox.local_part}@{mailbox.domain_name}</dd>
+                                <dt>{t('create_address_modal.success.credential_password')}</dt>
+                                <dd>{mailbox.one_time_password}</dd>
+                              </dl>
+                              <Button
+                                color="secondary"
+                                icon={<Icon name={clipboardState === 'copied' ? "check" : clipboardState === 'error' ? "close" : "content_copy"} />}
+                                onClick={handleCopyToClipboard}
+                              >
+                                {clipboardState === 'idle' && t('create_address_modal.success.copy_to_clipboard')}
+                                {clipboardState === 'copied' && t('create_address_modal.success.credentials_copied')}
+                                {clipboardState === 'error' && t('create_address_modal.success.credentials_copy_error')}
+                              </Button>
+                            </div>
                             <Banner type="warning" icon={<Icon name="info" type={IconType.OUTLINED} />}>
                               {t('create_address_modal.success.shared_password_info')}
                             </Banner>
