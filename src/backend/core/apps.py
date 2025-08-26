92f4d8fecda15c8a68a8110962841f2933a6e30e
Stanislas Bruhiere <stanislas@bruhiere.fr>
2025-08-26 11:47:30 +0200

    🚨(backend) ignore import-outside-toplevel for collector

--- src/backend/core/apps.py
+++ src/backend/core/apps.py
@@ -2,7 +2,7 @@
 
 from django.apps import AppConfig
 from django.utils.translation import gettext_lazy as _
-
+from prometheus_client.core import REGISTRY
 
 class CoreConfig(AppConfig):
     """Configuration class for the messages core app."""
@@ -12,11 +12,11 @@ class CoreConfig(AppConfig):
     verbose_name = _("messages core application")
 
     def ready(self):
-        """Register signal handlers when the app is ready."""
-        # Import signal handlers to register them
-        from prometheus_client.core import REGISTRY
-        from .metrics import CustomDBMetricsCollector
+        """Register signal handlers and prometheus collector when the app is ready."""
+        # pylint: disable=unused-import, import-outside-toplevel
+        from .metrics import CustomDBMetricsCollector  # noqa
         REGISTRY.register(CustomDBMetricsCollector())
 
+        # Import signal handlers to register them
         # pylint: disable=unused-import, import-outside-toplevel
         import core.signals  # noqa
