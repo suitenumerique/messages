487220d52ccedf04fad098ae0904ab7b59e92970
Stanislas Bruhiere <stanislas@bruhiere.fr>
2025-08-29 14:12:57 +0200

    ✨(backend) add healthcheck route, and suppress healthcheck access logs

--- src/backend/messages/wsgi.py
+++ src/backend/messages/wsgi.py
@@ -11,6 +11,21 @@ import os
 
 from configurations.wsgi import get_wsgi_application
 
+# Remove access logs for healthcheck route
+try:
+    import django.core.servers.basehttp
+
+    class QuietWSGIRequestHandler(django.core.servers.basehttp.WSGIRequestHandler):
+        def log_message(self, format, *args):
+            # # args[0] is the request line, e.g. 'GET /v1.0/healthz/ HTTP/1.1'
+            if len(args) > 0 and "/v1.0/healthz" in args[0]:
+                return
+            super().log_message(format, *args)
+
+    django.core.servers.basehttp.WSGIRequestHandler = QuietWSGIRequestHandler
+except ImportError:
+    pass
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "messages.settings")
 os.environ.setdefault("DJANGO_CONFIGURATION", "Development")
 
