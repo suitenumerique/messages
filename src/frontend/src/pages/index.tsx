786b7e8974bb1a9a2fce53c133bdfc9cde045a60
jbpenrath <jb.penrath@gmail.com>
2025-12-16 11:44:57 +0100

    🚀(frontend) allow to customize footer through envvars
    Production hotfix to be able to customize footer props without deploying the
    new layout.

--- src/frontend/src/pages/index.tsx
+++ src/frontend/src/pages/index.tsx
@@ -1,11 +1,24 @@
 import { useTranslation } from "react-i18next";
-import { Hero, HomeGutter, Footer, ProConnectButton } from "@gouvfr-lasuite/ui-kit";
+import { Hero, HomeGutter, Footer, ProConnectButton, FooterProps } from "@gouvfr-lasuite/ui-kit";
 import { login, useAuth } from "@/features/auth";
 import { MainLayout } from "@/features/layouts/components/main";
 import { LanguagePicker } from "@/features/layouts/components/main/language-picker";
 import { AppLayout } from "@/features/layouts/components/main/layout";
 import { LeftPanel } from "@/features/layouts/components/main/left-panel";
 import { FeedbackWidget } from "@/features/ui/components/feedback-widget";
+
+type ThemeConfig = {
+  theme?: string;
+  terms_of_service_url?: string;
+  footer?: FooterProps;
+};
+let THEME_CONFIG: ThemeConfig = {};
+
+try {
+  THEME_CONFIG = JSON.parse(process.env.NEXT_PUBLIC_THEME_CONFIG || '{}');
+} catch (error) {
+  console.error('Error parsing theme config', error);
+}
 
 export default function HomePage() {
 
@@ -34,7 +47,9 @@ export default function HomePage() {
             mainButton={<ProConnectButton onClick={login} />}
           />
         </HomeGutter>
-        <Footer />
+        {THEME_CONFIG.footer && (
+          <Footer {...THEME_CONFIG.footer} />
+        )}
       </div>
       <FeedbackWidget />
       </AppLayout>
