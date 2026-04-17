86ce1f15a79397bee608807cfa67f356a68bebeb
Bastien Ogier <bastien.ogier@ext.anct.gouv.fr>
2026-04-17 17:53:17 +0200

    (chore) bump keycloak to 26.6.1

--- src/keycloak/buildpack/scalingo_postcompile.sh
+++ src/keycloak/buildpack/scalingo_postcompile.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-KEYCLOAK_VERSION="26.5.4"
+KEYCLOAK_VERSION="26.6.1"
 KEYCLOAK_DIST="https://github.com/keycloak/keycloak/releases/download/${KEYCLOAK_VERSION}/keycloak-${KEYCLOAK_VERSION}.tar.gz"
 
 echo "-----> Downloading Keycloak $KEYCLOAK_VERSION"
