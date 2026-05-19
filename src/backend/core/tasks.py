b71b61cc534baf9bb641a4bc812feb6637220b69
jbpenrath <jb.penrath@gmail.com>
2026-05-19 10:02:24 +0200

    fixup! ✨(blobs) implement tiered storage and refactor blobs/attachments

--- src/backend/core/tasks.py
+++ src/backend/core/tasks.py
@@ -3,9 +3,11 @@
 
 from core.mda.inbound_tasks import *  # noqa: F403
 from core.mda.outbound_tasks import *  # noqa: F403
+from core.services.blob_gc import *  # noqa: F403
 from core.services.dns.tasks import *  # noqa: F403
 from core.services.importer.eml_tasks import *  # noqa: F403
 from core.services.importer.imap_tasks import *  # noqa: F403
 from core.services.importer.mbox_tasks import *  # noqa: F403
 from core.services.importer.pst_tasks import *  # noqa: F403
 from core.services.search.tasks import *  # noqa: F403
+from core.services.tiered_storage_tasks import *  # noqa: F403
